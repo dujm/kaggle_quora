33d0a18cc77bf9c56a925b486bbd846a9bcadead
dujm <jdu523@outlook.com>
2019-05-27 09:40:16 +0200

    format .py

--- src/models/01train-saved-model.py
+++ src/models/01train-saved-model.py
@@ -14,8 +14,24 @@ from tensorflow.python.keras.preprocessing.text import Tokenizer
 from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from tensorflow.python.keras.utils import to_categorical
 from tensorflow.python.keras.models import Sequential, Model
-from tensorflow.python.keras.layers import Dense, Input, LSTM, Embedding, Dropout, Activation, GRU, Conv1D,Bidirectional
-from tensorflow.python.keras import initializers, regularizers, constraints, optimizers, layers
+from tensorflow.python.keras.layers import (
+    Dense,
+    Input,
+    LSTM,
+    Embedding,
+    Dropout,
+    Activation,
+    GRU,
+    Conv1D,
+    Bidirectional,
+)
+from tensorflow.python.keras import (
+    initializers,
+    regularizers,
+    constraints,
+    optimizers,
+    layers,
+)
 
 from sklearn.model_selection import train_test_split
 from utils import standardize_text, text_to_array, batch_gen
@@ -33,7 +49,9 @@ def standardize_text(df, question_field):
     df[question_field] = df[question_field].str.replace(r"http\S+", "")
     df[question_field] = df[question_field].str.replace(r"http", "")
     df[question_field] = df[question_field].str.replace(r"@\S+", "")
-    df[question_field] = df[question_field].str.replace(r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " ")
+    df[question_field] = df[question_field].str.replace(
+        r"[^A-Za-z0-9(),!?@\'\`\"\_\n]", " "
+    )
     df[question_field] = df[question_field].str.replace(r"@", "at")
     df[question_field] = df[question_field].str.lower()
     return df
@@ -44,7 +62,7 @@ def text_to_array(text):
     empyt_emb = np.zeros(300)
     text = text[:-1].split()[:30]
     embeds = [embeddings_index.get(x, empyt_emb) for x in text]
-    embeds+= [empyt_emb] * (30 - len(embeds))
+    embeds += [empyt_emb] * (30 - len(embeds))
     return np.array(embeds)
 
 
@@ -53,11 +71,11 @@ os.chdir('/Users/j/Dropbox/Learn/kaggle_quora/src/')
 train_df = pd.read_csv("data/input/train.csv")
 print(train_df.head())
 
-test_df =pd.read_csv("data/input/test.csv")
+test_df = pd.read_csv("data/input/test.csv")
 print(test_df.head())
 
 
-#1. Preprocession: Lowercase, stemming, lemmarization, stopwords
+# 1. Preprocession: Lowercase, stemming, lemmarization, stopwords
 train_clean = train_df.copy(deep=True)
 test_clean = test_df.copy(deep=True)
 train_clean = standardize_text(train_clean, 'question_text')
@@ -81,64 +99,67 @@ for line in tqdm(f):
 f.close()
 
 print('Found %s word vectors.' % len(embeddings_index))
-#np.save('embeddings_index.npy', embeddings_index)
-#embeddings_index = np.load('embeddings_index.npy', allow_pickle=True)
+# np.save('embeddings_index.npy', embeddings_index)
+# embeddings_index = np.load('embeddings_index.npy', allow_pickle=True)
 
 
 # 3. text_to_array
 train_vects = [text_to_array(X_text) for X_text in tqdm(train_df["question_text"])]
 
-val_vects = np.array([text_to_array(X_text) for X_text in tqdm(val_df["question_text"][:3000])])
+val_vects = np.array(
+    [text_to_array(X_text) for X_text in tqdm(val_df["question_text"][:3000])]
+)
 
 val_y = np.array(val_df["target"][:3000])
-
 
 
 # %% {"_uuid": "c06041b7ee5792058819c138cc63b825cfa189d3"}
 checkpoint_path = "./model/training_1/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
-cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
-                                                 save_weights_only=True,
-                                                 verbose=1)
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    checkpoint_path, save_weights_only=True, verbose=1
+)
 
 # %% {"_uuid": "ef50d94d338ef4e7e2309ce12c49e0fb3b811578"}
 mg = batch_gen(train_df)
+
+
 def create_model():
     model = Sequential()
-    model.add(Bidirectional(LSTM(64, return_sequences=True),
-                        input_shape=(30, 300)))
+    model.add(Bidirectional(LSTM(64, return_sequences=True), input_shape=(30, 300)))
     model.add(Bidirectional(LSTM(64)))
     model.add(Dense(1, activation="sigmoid"))
-    model.compile(loss='binary_crossentropy',
-              optimizer='adam',
-              metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
-model =create_model()
-model.fit_generator(mg, epochs=5,
-                    steps_per_epoch=1000,
-                    validation_data=(val_vects, val_y),
-                    calllbacks =[cp_callback])
+
+
+model = create_model()
+model.fit_generator(
+    mg,
+    epochs=5,
+    steps_per_epoch=1000,
+    validation_data=(val_vects, val_y),
+    calllbacks=[cp_callback],
+)
 model.summary()
 
 loss, acc = model.evaluate(val_vects, val_y)
-print("Untrained model, accuracy: {:5.2f}%".format(100*acc))
+print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
 # Untrained model, accuracy: 96.43%
 
 # %% TF 2.0 saved_model
 # https://www.tensorflow.org/alpha/guide/saved_model
 # https://www.tensorflow.org/versions/r2.0/api_docs/python/tf/saved_model/save
 tf.saved_model.save(model, "sincere/1/")
-saved_model_cli show --dir sincere/1 --tag_set serve --signature_def serving_default
+# saved_model_cli show --dir sincere/1 --tag_set serve --signature_def serving_default
 
 # %%
 # If you want to save the weights manually
 # https://www.tensorflow.org/tutorials/keras/save_and_restore_models
-#model.save("../models/tf-df/model{}.h5".format(int(time.time())))
-#model.save_weights("../models/tf-df/weights{}".format(int(time.time())))
-
-
+# model.save("../models/tf-df/model{}.h5".format(int(time.time())))
+# model.save_weights("../models/tf-df/weights{}".format(int(time.time())))
 
 
 # Restore the weights
-#model = create_model()
-#model.load_weights('models/tf-df/weights1557089352.index')
+# model = create_model()
+# model.load_weights('models/tf-df/weights1557089352.index')
